c052cf8ea8eea2090d0c5e6ecb9f5895b8c51d58
Barbara Okwufulueze <barbaraokwufulueze@gmail.com>
2022-03-25 11:18:07 +0100

    Add Shopping List feature

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,6 +1,6 @@
 import icons from 'url:../../img/icons.svg';
 import { Fraction } from 'fractional';
-import View from './View';
+import View from './view.js';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -1,4 +1,4 @@
-import View from './View.js';
+import View from './view.js';
 import previewView from './previewView.js';
 
 class ResultsView extends View {
